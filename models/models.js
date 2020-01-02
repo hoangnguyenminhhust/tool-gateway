a6f908a8c728f31160a4f0b8ed58d871cd2e266e
Nguyễn Minh Hoàng <hoangnguyenminh.hust@gmail.com>
2020-01-02 11:56:01 +0700

    hoang

--- models/models.js
+++ models/models.js
@@ -2,14 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let ApiSchema = new Schema({
-	config :  { type: Object,
-		unique : true,
-
-		method : String,
-		path: String, 
+	name : String ,
+	path: String,
+	authentication: {
+		type: Boolean,
+		default : false
 	},
-	handler : String
-	
+	authorziration : {
+		type: Boolean,
+		default: false
+	},
+	Endpoint :[
+		{	
+			_id: new Schema.Types.ObjectId,
+			method : String,
+			path: String, 
+			handler : String,
+		}
+	]
 });
 
 module.exports = mongoose.model("Api" , ApiSchema );
